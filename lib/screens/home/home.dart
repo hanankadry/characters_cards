5955527219a92a0e9d9c00bc3928e481e3d3aec6
Hanan <hanan.kadry@outlook.com>
2026-02-12 22:37:02 +0200

    adding models

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,3 +1,4 @@
+import 'package:characters_cards/shared/styled_button.dart';
 import 'package:characters_cards/shared/styled_text.dart';
 import 'package:flutter/material.dart';
 
@@ -9,6 +10,8 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List characters = ['mario', 'luigi', 'peach', 'toad', 'bowser', 'koopa'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,17 +23,28 @@ class _HomeState extends State<Home> {
         padding: const EdgeInsets.all(16),
         child: Column(
           children: [
-            const StyledTitle('title'),
-            const StyledHeading('heading'),
-            const StyledText('text'),
-            
-            FilledButton(
+            // list of characters
+            Expanded(
+              child: ListView.builder(
+                itemCount: characters.length,
+                itemBuilder: (_, index) {
+                  return Container(
+                    color: Colors.grey[800],
+                    padding: const EdgeInsets.all(40),
+                    margin: const EdgeInsets.only(bottom: 40),
+                    child: Text(characters[index]),
+                  );
+                },
+              ),
+            ),
+
+            StyledButton(
               onPressed: () {
                 // navigate to the create screen
               },
-              child: const Text('Create New'),
+              child: const StyledHeading('Create New'),
             ),
-          ]
+          ],
         ),
       ),
     );
