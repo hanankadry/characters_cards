60ba681a145296cd71ce71ad4873739366cbafa7
Hanan <hanan.kadry@outlook.com>
2026-02-12 22:19:31 +0200

    setting up

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,22 @@
+import 'package:characters_cards/screens/home/home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp());
+  runApp(const MaterialApp(home: Home()));
+}
+
+// sandbox
+class Sandbox extends StatelessWidget {
+  const Sandbox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sandbox'),
+        backgroundColor: Colors.grey,
+      ),
+      body: const Text('sandbox'),
+    );
+  }
 }
